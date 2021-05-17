17a21b5b3ec2aa9dc400dc0aaa15d2764a155b4a
akash <akash@MSI>
2021-05-17 17:41:09 +0530

    create a queue

---
+++ main/java/MyQueue.java
@@ -0,0 +1,15 @@
+public class MyQueue {
+    private final MyLinkedList myLinkedList;
+
+    public MyQueue() {
+        this.myLinkedList = new MyLinkedList();
+    }
+
+    public void enqueue(INode myNode) {
+        myLinkedList.append(myNode);
+    }
+
+    public void printQueue() {
+        myLinkedList.printMyNodes();
+    }
+}
