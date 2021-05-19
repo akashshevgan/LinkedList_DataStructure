bacab397c1ec15b3be22e7a48a5f2942ab01dbf5
akash <akash@MSI>
2021-05-19 18:04:11 +0530

    Added : counting frequency of word in hashmap

--- main/java/MyLinkedList.java
+++ main/java/MyLinkedList.java
@@ -1,120 +1,132 @@
 public class MyLinkedList<K extends Comparable> {
 
-    public INode head;
-    public INode tail;
+        public INode head;
+        public INode tail;
 
-    public MyLinkedList() {
-        this.head = null;
-        this.tail = null;
-    }
-
-    public void add(INode newNode) {
-        if (this.tail == null)
-            this.tail = newNode;
-        if (this.head == null)
-            this.head = newNode;
-        else {
-            INode tempNode = this.head;
-            this.head = newNode;
-            this.head.setNext(tempNode);
+        public MyLinkedList() {
+            this.head = null;
+            this.tail = null;
         }
-    }
 
-//print linked list
-    public void printMyNodes(){
-        StringBuffer myNodes = new StringBuffer("My Nodes: ");
-        INode tempNode = head;
-        while (tempNode.getNext()!=null){
+        public void add(INode newNode) {
+            if (this.tail == null)
+                this.tail = newNode;
+            if (this.head == null)
+                this.head = newNode;
+            else {
+                INode tempNode = this.head;
+                this.head = newNode;
+                this.head.setNext(tempNode);
+            }
+        }
+
+        //print linked list
+        public void printMyNodes() {
+            StringBuffer myNodes = new StringBuffer("My Nodes: ");
+            INode tempNode = head;
+            while (tempNode.getNext() != null) {
+                myNodes.append(tempNode.getKey());
+                if (!tempNode.equals(tail))
+                    myNodes.append("->");
+                tempNode = tempNode.getNext();
+            }
             myNodes.append(tempNode.getKey());
-            if(!tempNode.equals(tail))
-                myNodes.append("->");
-            tempNode=tempNode.getNext();
+            System.out.println(myNodes);
         }
-        myNodes.append(tempNode.getKey());
-        System.out.println(myNodes);
-    }
 
-    public void append(INode newNode) {
-        if(this.head == null)
-            this.head = newNode;
-        if(this.tail == null)
-            this.tail = newNode;
-        else {
-            INode tempNode = this.tail;
-            this.tail = newNode;
-            tempNode.setNext(newNode);
+        public void append(INode newNode) {
+            if (this.head == null)
+                this.head = newNode;
+            if (this.tail == null)
+                this.tail = newNode;
+            else {
+                INode tempNode = this.tail;
+                this.tail = newNode;
+                tempNode.setNext(newNode);
+            }
         }
-    }
 
-    public void insert(INode myNode, INode newNode) {
-        this.head.setNext(myNode);
-        myNode.setNext(newNode);
-    }
+        public void insert(INode myNode, INode newNode) {
+            this.head.setNext(myNode);
+            myNode.setNext(newNode);
+        }
 
-    //delete first element in linkedlist
-    public INode pop() {
-        INode  tempNode = this.head;
-        this.head = head.getNext();
-        return tempNode;
-    }
+    public INode<K> searchKey(K key) {
+        INode<K> tempNode = head;
+        while (tempNode != null && tempNode.getNext() != null){
+            if(tempNode.getKey().equals((K) key)){
+                return tempNode;
+            }
 
-    public INode popLast() {
-        INode tempNode = head;
-        while (!tempNode.getNext().equals(tail)) {
             tempNode = tempNode.getNext();
         }
-        this.tail = tempNode;
-        tempNode = null;
-        return tempNode;
+        return null;
     }
 
-    public void search(INode myNode){   //  search given element
-        INode tempNode = head;
-        while (tempNode.getNext()!=myNode){
-            tempNode=tempNode.getNext();
+        //delete first element in linkedlist
+        public INode pop() {
+            INode tempNode = this.head;
+            this.head = head.getNext();
+            return tempNode;
         }
-        tempNode = tempNode.getNext();
-        System.out.println(" Element is = " +tempNode.getKey());
-    }
 
-    public void delete(INode deleteNode) {
-        INode tempNode1 = head;
-        while (tempNode1.getNext() != deleteNode) {
+        public INode popLast() {
+            INode tempNode = head;
+            while (!tempNode.getNext().equals(tail)) {
+                tempNode = tempNode.getNext();
+            }
+            this.tail = tempNode;
+            tempNode = null;
+            return tempNode;
+        }
+
+        public void search(INode myNode) {   //  search given element
+            INode tempNode = head;
+            while (tempNode.getNext() != myNode) {
+                tempNode = tempNode.getNext();
+            }
+            tempNode = tempNode.getNext();
+            System.out.println(" Element is = " + tempNode.getKey());
+        }
+
+        public void delete(INode deleteNode) {
+            INode tempNode1 = head;
+            while (tempNode1.getNext() != deleteNode) {
+                tempNode1 = tempNode1.getNext();
+            }
+            INode tempNode2 = tempNode1;
             tempNode1 = tempNode1.getNext();
+            tempNode1 = tempNode1.getNext();
+            tempNode2.setNext(tempNode1);
         }
-        INode tempNode2 = tempNode1;
-        tempNode1 = tempNode1.getNext();
-        tempNode1 = tempNode1.getNext();
-        tempNode2.setNext(tempNode1);
-    }
 
-    public void size(INode head) {
-        int count = 0;
-        INode node = head;
-        while (node != null) {
-            count++;
-            node = node.getNext();
+        public void size(INode head) {
+            int count = 0;
+            INode node = head;
+            while (node != null) {
+                count++;
+                node = node.getNext();
+            }
+            System.out.println("Size : " + count);
         }
-        System.out.println("Size : " + count);
-    }
-    // uc 10 Sorted LinkedList
-    public void sortedLinkedList(INode<K> newNode) {
-        INode tempNode = head;
-        INode prevNode = null;
-        while (tempNode != null && (newNode.getKey()).compareTo((K) tempNode.getKey()) > 0) {
-            prevNode = tempNode;
-            tempNode = tempNode.getNext();
+
+        public void sortedLinkedList(INode<K> newNode) {
+            INode tempNode = head;
+            INode prevNode = null;
+            while (tempNode != null && (newNode.getKey()).compareTo((K) tempNode.getKey()) > 0) {
+                prevNode = tempNode;
+                tempNode = tempNode.getNext();
+            }
+            if (prevNode == null) {
+                this.head = newNode;
+            } else {
+                prevNode.setNext(newNode);
+            }
+            newNode.setNext(tempNode);
+            tempNode = head;
+            while (tempNode != null) {
+                this.tail = tempNode;
+                tempNode = tempNode.getNext();
+            }
         }
-        if (prevNode == null) {
-            this.head = newNode;
-        } else {
-            prevNode.setNext(newNode);
-        }
-        newNode.setNext(tempNode);
-        tempNode =head;
-        while (tempNode!=null){
-            this.tail=tempNode;
-            tempNode=tempNode.getNext();
-        }
-    }
 }
